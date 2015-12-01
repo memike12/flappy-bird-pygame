f998968c23afc5980df9cefda0b3a7828cb6db42
Mike <memike12@gmail.com>
2015-12-01 10:49:43 -0500

    making moves

--- flappybird.py
+++ flappybird.py
@@ -16,7 +16,6 @@ ANIMATION_SPEED = 0.18  # pixels per millisecond
 WIN_WIDTH = 284 * 2     # BG image size: 284x512 px; tiled twice
 WIN_HEIGHT = 512
 
-
 class Bird(pygame.sprite.Sprite):
     """Represents the bird controlled by the player.
 
@@ -85,13 +84,18 @@ class Bird(pygame.sprite.Sprite):
         delta_frames: The number of frames elapsed since this method was
             last called.
         """
+        #start = 0
+        
         if self.msec_to_climb > 0:
             frac_climb_done = 1 - self.msec_to_climb/Bird.CLIMB_DURATION
             self.y -= (Bird.CLIMB_SPEED * frames_to_msec(delta_frames) *
                        (1 - math.cos(frac_climb_done * math.pi)))
             self.msec_to_climb -= frames_to_msec(delta_frames)
+        #    if self.y > start:
+        #        start = self.y
         else:
             self.y += Bird.SINK_SPEED * frames_to_msec(delta_frames)
+        #    print (start - self.y , self.x)
 
     @property
     def image(self):
@@ -309,80 +313,120 @@ def main():
     If someone executes this module (instead of importing it, for
     example), this function is called.
     """
-
+    finished = False
+    highscore = 0
     pygame.init()
+    while not finished:
+        display_surface = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        pygame.display.set_caption('Pygame Flappy Bird')
 
-    display_surface = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-    pygame.display.set_caption('Pygame Flappy Bird')
+        clock = pygame.time.Clock()
+        score_font = pygame.font.SysFont(None, 32, bold=True)  # default font
+        top_font = pygame.font.SysFont(None, 32, bold=True)  # default font
+        bottom_font = pygame.font.SysFont(None, 32, bold=True)  # default font
+        btop_font = pygame.font.SysFont(None, 32, bold=True)  # default font
+        bbot_font = pygame.font.SysFont(None, 32, bold=True)  # default font
+        pipe_font = pygame.font.SysFont(None, 32, bold=True)  # default font
+        images = load_images()
 
-    clock = pygame.time.Clock()
-    score_font = pygame.font.SysFont(None, 32, bold=True)  # default font
-    images = load_images()
+        # the bird stays in the same x position, so bird.x is a constant
+        # center bird on screen
+        bird = Bird(50, int(WIN_HEIGHT/2 - Bird.HEIGHT/2), 2,
+                    (images['bird-wingup'], images['bird-wingdown']))
 
-    # the bird stays in the same x position, so bird.x is a constant
-    # center bird on screen
-    bird = Bird(50, int(WIN_HEIGHT/2 - Bird.HEIGHT/2), 2,
-                (images['bird-wingup'], images['bird-wingdown']))
+        pipes = deque()
 
-    pipes = deque()
+        frame_clock = 0  # this counter is only incremented if the game isn't paused
+        score = 0
+        done = paused = False
+        while not done:
+            clock.tick(FPS)
 
-    frame_clock = 0  # this counter is only incremented if the game isn't paused
-    score = 0
-    done = paused = False
-    while not done:
-        clock.tick(FPS)
+            # Handle this 'manually'.  If we used pygame.time.set_timer(),
+            # pipe addition would be messed up when paused.
+            if not (paused or frame_clock % msec_to_frames(PipePair.ADD_INTERVAL)):
+                pp = PipePair(images['pipe-end'], images['pipe-body'])
+                pipes.append(pp)
 
-        # Handle this 'manually'.  If we used pygame.time.set_timer(),
-        # pipe addition would be messed up when paused.
-        if not (paused or frame_clock % msec_to_frames(PipePair.ADD_INTERVAL)):
-            pp = PipePair(images['pipe-end'], images['pipe-body'])
-            pipes.append(pp)
+            for e in pygame.event.get():
+                if e.type == QUIT or (e.type == KEYUP and e.key == K_ESCAPE):
+                    finished = True
+                    done = True
+                    break
+                elif e.type == KEYUP and e.key in (K_PAUSE, K_p):
+                    paused = not paused
+                elif e.type == MOUSEBUTTONUP or (e.type == KEYUP and
+                        e.key in (K_UP, K_RETURN, K_SPACE)):
+                    bird.msec_to_climb = Bird.CLIMB_DURATION
 
-        for e in pygame.event.get():
-            if e.type == QUIT or (e.type == KEYUP and e.key == K_ESCAPE):
+            if paused:
+                continue  # don't draw anything
+
+            # check for collisions
+            pipe_collision = any(p.collides_with(bird) for p in pipes)
+            if pipe_collision or 0 >= bird.y or bird.y >= WIN_HEIGHT - Bird.HEIGHT:
                 done = True
-                break
-            elif e.type == KEYUP and e.key in (K_PAUSE, K_p):
-                paused = not paused
-            elif e.type == MOUSEBUTTONUP or (e.type == KEYUP and
-                    e.key in (K_UP, K_RETURN, K_SPACE)):
-                bird.msec_to_climb = Bird.CLIMB_DURATION
 
-        if paused:
-            continue  # don't draw anything
+            for x in (0, WIN_WIDTH / 2):
+                display_surface.blit(images['background'], (x, 0))
 
-        # check for collisions
-        pipe_collision = any(p.collides_with(bird) for p in pipes)
-        if pipe_collision or 0 >= bird.y or bird.y >= WIN_HEIGHT - Bird.HEIGHT:
-            done = True
+            while pipes and not pipes[0].visible:
+                pipes.popleft()
 
-        for x in (0, WIN_WIDTH / 2):
-            display_surface.blit(images['background'], (x, 0))
+            for p in pipes:
+                p.update()
+                display_surface.blit(p.image, p.rect)
 
-        while pipes and not pipes[0].visible:
-            pipes.popleft()
+            bird.update()
+            display_surface.blit(bird.image, bird.rect)
 
-        for p in pipes:
-            p.update()
-            display_surface.blit(p.image, p.rect)
+            # update and display score
+            for p in pipes:
+                if p.x + PipePair.WIDTH < bird.x and not p.score_counted:
+                    score += 1
+                    p.score_counted = True
 
-        bird.update()
-        display_surface.blit(bird.image, bird.rect)
+            score_surface = score_font.render(str(score), True, (255, 255, 255))
+            score_x = WIN_WIDTH/2 - score_surface.get_width()/2
+            display_surface.blit(score_surface, (score_x, PipePair.PIECE_HEIGHT))
 
-        # update and display score
-        for p in pipes:
-            if p.x + PipePair.WIDTH < bird.x and not p.score_counted:
-                score += 1
-                p.score_counted = True
+            # display bird position
+            top_surface = top_font.render("top :" + str(p.top_height_px), True, (255, 255, 255))
+            top_x = WIN_WIDTH/2 - top_surface.get_width()/2 - 150
+            #display_surface.blit(top_surface, (top_x, PipePair.PIECE_HEIGHT))
 
-        score_surface = score_font.render(str(score), True, (255, 255, 255))
-        score_x = WIN_WIDTH/2 - score_surface.get_width()/2
-        display_surface.blit(score_surface, (score_x, PipePair.PIECE_HEIGHT))
+            bottom_surface = bottom_font.render("bottom :" +str(WIN_HEIGHT - p.bottom_height_px), True, (255, 255, 255))
+            bottom_x = WIN_WIDTH/2 - bottom_surface.get_width()/2 - 150
+            #display_surface.blit(bottom_surface, (bottom_x, PipePair.PIECE_HEIGHT + 25 ))
 
-        pygame.display.flip()
-        frame_clock += 1
-    print('Game over! Score: %i' % score)
+            #logic to make sure your switching pipes once you're clear of the close one
+            if (pipes[0].rect[0] + 80 - bird.x) < 0:
+                closestPipe = 1
+            else:
+                closestPipe = 0
+            pipe_surface = pipe_font.render("pipe x:" +str(pipes[closestPipe].rect[0]), True, (255, 255, 255))
+            pipe_x = WIN_WIDTH/2 - pipe_surface.get_width()/2
+            display_surface.blit(pipe_surface, (pipe_x, PipePair.PIECE_HEIGHT + 25 ))
+
+            btop_surface = btop_font.render("bird top :" +str(int(bird.y)), True, (255, 255, 255))
+            btop_x = WIN_WIDTH/2 - btop_surface.get_width()/2 + 180
+            #display_surface.blit(btop_surface, (btop_x, PipePair.PIECE_HEIGHT))
+
+            bbot_surface = bbot_font.render("bird bottom :" +str(int(bird.y)+32), True, (255, 255, 255))
+            bbot_x = WIN_WIDTH/2 - bbot_surface.get_width()/2 + 180
+            #display_surface.blit(bbot_surface, (bbot_x, PipePair.PIECE_HEIGHT + 25 ))
+
+            pygame.display.flip()
+            frame_clock += 1
+        print('Game over! Score: %i' % score)
+        if score >= highscore:
+            highscore = score
+            pygame.image.save(display_surface,"highscores/" + str(score) + ".jpg")
+        print('pipe left: ' + str(pipes[0].rect[0]) + ' \tright: ' + str(pipes[0].rect[0] + 80) + ' \ttop: ' + str(p.top_height_px) + ' \tbottom: ' + str(WIN_HEIGHT - p.bottom_height_px))
+        print('bird left: ' + str(int(bird.x))  + ' \tright: ' + str(int(bird.x+32)) + ' \ttop: ' + str(int(bird.y)) + ' \tbottom: ' + str(int(bird.y+32)))
+        #print(WIN_HEIGHT - p.bottom_height_px -p.top_height_px)  #distance between pipes is 128 px
     pygame.quit()
+
 
 
 if __name__ == '__main__':
